05561fc5402c2654ea8d025549ff3915ec01706a
Hacker6329 <Hacker6329@gmail.com>
2021-07-29 13:43:42 +0200

    Updated QuarryTileEntity.java

--- src/main/java/com/italiandudes/projectspace/tileEntity/QuarryTileEntity.java
+++ src/main/java/com/italiandudes/projectspace/tileEntity/QuarryTileEntity.java
@@ -1,10 +1,15 @@
 package com.italiandudes.projectspace.tileEntity;
 
 import com.italiandudes.projectspace.init.ModTileEntityTypes;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.Objects;
 
 public class QuarryTileEntity extends TileEntity implements ITickableTileEntity {
     public QuarryTileEntity(TileEntityType<?> tileEntityTypeIn) {
@@ -18,6 +23,8 @@ public class QuarryTileEntity extends TileEntity implements ITickableTileEntity 
     //This method will run every single time the world ticks!
     @Override
     public void tick() {
-        this.level.setBlock(this.worldPosition.below(),Blocks.AIR.defaultBlockState(),1);
+        if (Blocks.BEDROCK.defaultBlockState() == Objects.requireNonNull(this.level).getBlockState(this.worldPosition.below())) {
+            this.level.setBlock(this.worldPosition.below(), Blocks.AIR.defaultBlockState(), 1);
+        }
     }
 }
