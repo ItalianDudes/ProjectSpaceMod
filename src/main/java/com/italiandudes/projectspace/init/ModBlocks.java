7a6c00c18d380a9c5955e9ec8eb029753387e143
diego-graziati <dgraziati@gmail.com>
2021-08-05 14:25:35 +0200

    Standardized block initiated.

--- src/main/java/com/italiandudes/projectspace/init/ModBlocks.java
+++ src/main/java/com/italiandudes/projectspace/init/ModBlocks.java
@@ -16,8 +16,8 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ProjectSpace.MOD_ID);
 
     //Under here are listed all the blocks.
-    public static final RegistryObject<Block> QUARRY = BLOCKS.register("quarry", () -> new QuarryBlock());
-    public static final RegistryObject<Block> DISPLAY_CASE = BLOCKS.register("display_case", () -> new DisplayCaseBlock());
+    public static final RegistryObject<Block> QUARRY = BLOCKS.register("quarry", QuarryBlock::new);
+    public static final RegistryObject<Block> DISPLAY_CASE = BLOCKS.register("display_case", DisplayCaseBlock::new);
 
     //Under here are listed all the fluid's blocks.
     public static final RegistryObject<FlowingFluidBlock> DEBUG_BLOCK = BLOCKS.register("debug_fluid",
