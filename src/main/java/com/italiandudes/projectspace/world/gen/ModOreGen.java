19bc8174815988be160a9c3d56a1b81dccd24398
diego-graziati <dgraziati@gmail.com>
2021-08-11 15:27:49 +0200

    Added all ores loot_tables

---
+++ src/main/java/com/italiandudes/projectspace/world/gen/ModOreGen.java
@@ -0,0 +1,15 @@
+package com.italiandudes.projectspace.world.gen;
+
+import com.italiandudes.projectspace.ProjectSpace;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod.EventBusSubscriber(modid = ProjectSpace.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModOreGen {
+
+    @SubscribeEvent
+    public static void generateOres(FMLLoadCompleteEvent event){
+
+    }
+}
