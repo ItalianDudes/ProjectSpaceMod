910faa954cdc035ac16817bc98f4ece435ea4da4
diego-graziati <dgraziati@gmail.com>
2021-08-12 19:10:33 +0200

    Fixing the cells' problems.

--- src/main/java/com/italiandudes/projectspace/container/DebugDisplayCaseContainers.java
+++ src/main/java/com/italiandudes/projectspace/container/DebugDisplayCaseContainers.java
@@ -32,7 +32,7 @@ public class DebugDisplayCaseContainers extends Container {
         //Main Player Inventory
         for(int row=0; row<3; row++){
             for(int col=0;col<9;col++){
-                this.addSlot(new Slot(playerInv, col+row*9+9,8*col*18,166-(4-row)*18-10));
+                this.addSlot(new Slot(playerInv, col+row*9+9,8+col*18,166-(4-row)*18-10));
             }
         }
 
