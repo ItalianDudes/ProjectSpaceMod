becd82b63cb02d0b90f080dc471c156615d2507d
diego-graziati <dgraziati@gmail.com>
2021-08-13 08:45:13 +0200

    Successfully added the TileEntityRender for the DebugDisplayCase.

--- src/main/java/com/italiandudes/projectspace/client/tileEntityRenderer/DebugDisplayCaseTileEntityRenderer.java
+++ src/main/java/com/italiandudes/projectspace/client/tileEntityRenderer/DebugDisplayCaseTileEntityRenderer.java
@@ -4,6 +4,7 @@ import com.italiandudes.projectspace.tileEntity.DebugDisplayCaseTileEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.model.IBakedModel;
@@ -11,10 +12,13 @@ import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Quaternion;
-import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 
@@ -31,7 +35,7 @@ public class DebugDisplayCaseTileEntityRenderer extends TileEntityRenderer<Debug
     @Override
     public void render(DebugDisplayCaseTileEntity caseTileEntity, float partialTicks, MatrixStack matrixStack,
                        IRenderTypeBuffer buffer, int combinedLightIn, int combinedOverlayIn) {
-        if (caseTileEntity.getItem().equals(ItemStack.EMPTY)){
+        if (caseTileEntity.getItem().equals(ItemStack.EMPTY) || caseTileEntity.getItem().equals(Items.AIR)){
             return;
         }
 
@@ -40,6 +44,10 @@ public class DebugDisplayCaseTileEntityRenderer extends TileEntityRenderer<Debug
         renderItem(caseTileEntity.getItem(),new double[] {0.5d,1d,0.5d},
                 Vector3f.YP.rotationDegrees(180f - player.yRot),matrixStack, buffer, partialTicks,
                 combinedOverlayIn,lightLevel,0.8f);
+
+        ITextComponent label = caseTileEntity.getItem().hasCustomHoverName() ? caseTileEntity.getItem().getDisplayName() : new TranslationTextComponent(caseTileEntity.getItem().getDescriptionId());
+
+        renderLabel(matrixStack,buffer,lightLevel,new double[] {.5d,1.3d,.5d}, label,0xffffff);
     }
 
     private void renderItem(ItemStack stack, double[] translation, Quaternion rotation, MatrixStack matrixStack, IRenderTypeBuffer buffer,
@@ -53,6 +61,26 @@ public class DebugDisplayCaseTileEntityRenderer extends TileEntityRenderer<Debug
         mc.getItemRenderer().render(stack, ItemCameraTransforms.TransformType.GROUND,true,matrixStack,buffer,
                 lightLevel,combinedOverlay,model);
         matrixStack.popPose();
+    }
+
+    private void renderLabel(MatrixStack stack, IRenderTypeBuffer buffer, int lightLevel, double[] corner, ITextComponent text,
+                             int color){
+
+        FontRenderer font = mc.font;
+
+        stack.pushPose();
+        float scale = 0.01f;
+        int opacity = (int) (.4*255.0f) << 24;
+        float offset = (float) (-font.width(text)/2);
+        Matrix4f matrix = stack.last().pose();
+
+        stack.translate(corner[0],corner[1] + .4f,corner[2]);
+        stack.scale(scale,scale,scale);
+        stack.mulPose(mc.getEntityRenderDispatcher().cameraOrientation());
+        stack.mulPose(Vector3f.ZP.rotationDegrees(180f));
+
+        font.drawInBatch(text,offset,0,color,false,matrix,buffer,false,opacity,lightLevel);
+        stack.popPose();
     }
 
     @SuppressWarnings("deprecation")
--- src/main/java/com/italiandudes/projectspace/util/ClientEventBusSubscriber.java
+++ src/main/java/com/italiandudes/projectspace/util/ClientEventBusSubscriber.java
@@ -2,10 +2,13 @@ package com.italiandudes.projectspace.util;
 
 import com.italiandudes.projectspace.ProjectSpace;
 import com.italiandudes.projectspace.client.gui.DebugDisplayCaseGUI;
+import com.italiandudes.projectspace.client.tileEntityRenderer.DebugDisplayCaseTileEntityRenderer;
 import com.italiandudes.projectspace.init.ModContainerTypes;
+import com.italiandudes.projectspace.init.ModTileEntityTypes;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -15,5 +18,7 @@ public class ClientEventBusSubscriber {
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event){
         ScreenManager.register(ModContainerTypes.DEBUG_DISPLAY_CASE_CONTAINERS_TYPE.get(), DebugDisplayCaseGUI::new);
+
+        ClientRegistry.bindTileEntityRenderer(ModTileEntityTypes.DEBUG_DISPLAY_CASE_TILE_ENTITY_TYPE.get(), DebugDisplayCaseTileEntityRenderer::new);
     }
 }
