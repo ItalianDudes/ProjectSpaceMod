609366e7ee5ebc486234d11538fc3a5159141b3d
diego-graziati <dgraziati@gmail.com>
2021-10-13 18:59:08 +0200

    Partial fix. The game doesn't crash anymore. The block is loaded in the game, the model isn't.

--- src/main/java/com/italiandudes/projectspace/blocks/DebugBlock.java
+++ src/main/java/com/italiandudes/projectspace/blocks/DebugBlock.java
@@ -1,29 +1,13 @@
 package com.italiandudes.projectspace.blocks;
 
-import com.italiandudes.projectspace.ProjectSpace;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.obj.OBJLoader;
-import net.minecraftforge.client.model.obj.OBJModel;
 
 public class DebugBlock extends Block {
 
-    OBJLoader loader = new OBJLoader();
-
-    public void setLoader(OBJLoader loader) {
-        this.loader = loader;
-        ResourceLocation location = new ResourceLocation(ProjectSpace.MOD_ID,"block/debug_test.obj");
-        String matLocation = new String("block/debug_test.mtl");
-
-        loader.loadModel(new OBJModel.ModelSettings(location,true,true, false, true,matLocation));
-    }
-
     public DebugBlock() {
         super(AbstractBlock.Properties.of(Material.METAL));
-
-        setLoader(new OBJLoader());
     }
 
 }
