398052d52973998577f3c5b02b34da538a0fe2b5
Hacker6329 <Hacker6329@gmail.com>
2021-10-21 01:17:13 +0200

    Attempting to introduce sounds

---
+++ src/main/java/com/italiandudes/projectspace/init/ModSoundEvents.java
@@ -0,0 +1,20 @@
+package com.italiandudes.projectspace.init;
+
+import com.italiandudes.projectspace.ProjectSpace;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModSoundEvents {
+
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, ProjectSpace.MOD_ID);
+
+    private static RegistryObject<SoundEvent> registerSoundEvent(String name){
+        return SOUND_EVENTS.register(name, ()-> new SoundEvent(new ResourceLocation(ProjectSpace.MOD_ID,name)));
+    }
+
+    //Under here are listed all the sounds.
+    public static final RegistryObject<SoundEvent> DEBUG_SOUND = registerSoundEvent("debug_sound");
+}
